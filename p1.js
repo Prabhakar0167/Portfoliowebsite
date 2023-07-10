7d67bbcfaf0853998e0818757792d9171eac7b05
Prabhakar0167 <138846873+Prabhakar0167@users.noreply.github.com>
2023-07-10 10:23:59 +0530

    Update and rename p1.js to main.js

--- p1.js
+++
@@ -1,7 +0,0 @@
-let menuIcon = document.querySelector('#menu-icon');
-let navlist = document.querySelector('.navlist');
-
-menuIcon.onclick = () => {
-    menuIcon.classList.toggle('bx-x');
-    navlist.classList.toggle('active');
-}
